11ddb7562712f5228951d74a656fcade7dfff81a
coffe <coffeacc10@gmail.com>
2018-03-18 11:33:55 +0200

    Added Screenshots

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -2,11 +2,21 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Story;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 import org.testng.ITestNGListener;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static java.lang.String.format;
+import static org.testng.Assert.fail;
 
 
 @Epic("Regression10Tests")
@@ -25,20 +35,37 @@ public class SimpleTest {
     @Story("My story")
 
     @Test(priority = 3)
-    public void getGoogle() throws InterruptedException {
+    public void getGoogle()  {
         driver.get("Https:Google.com");
-        Thread.sleep(5000);
+        saveScreenshotPNG(driver);
     }
 
     @Test(priority = 2)
-    public void getFacebook() throws InterruptedException {
+    public void getFacebook()   {
         driver.get("Https:facebook.com");
-Thread.sleep(5000);
+saveScreenshotPNG(driver);
+saveTextLog("Here you will find Success work");
+
+
+    }
+
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG (WebDriver driver) {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+    //Text attachments for Allure
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog (String message) {
+        return message;
+    }
+
+    //HTML attachments for Allure
+    @Attachment(value = "{0}", type = "text/html")
+    public static String attachHtml(String html) {
+        return html;
     }
 
 
-
-   
 
 
 
